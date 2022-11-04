da173383cebc09f0895e78ff04974bf6ff3a330c
anthobab <anthony.babin1@gmail.com>
2022-11-04 09:40:31 +0100

    Done (still the css to improve)

--- app.js
+++ app.js
@@ -14,12 +14,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  const beersArray = await punkAPI.getBeers();
+
+  console.log(beersArray);
+  res.render('beers', { beersArray: beersArray });
+});
+
+// app.get('/beers', (req, res) => {
+//   punkAPI
+//     .getBeers()
+//     .then(beersFromApi => {
+//       res.render('beers', { beersArray: beersFromApi });
+//     })
+//     .catch(error => console.log(error));
+// });
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom();
+
+  console.log(randomBeer);
+  res.render('random-beer', { randomBeer: randomBeer });
+});
+
+app.get('/beers/beer-:id', async (req, res) => {
+  res.render('random-beer', {
+    randomBeer: await punkAPI.getBeer(req.params.id)
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
